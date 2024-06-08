c6aa38770748badc53ead9ed1c518485f3657437
zk.gao <1054115624@qq.com>
2024-06-08 19:38:07 +0800

    add some image and primeIcons

--- components/side.vue
+++ components/side.vue
@@ -1,20 +1,65 @@
 <template>
     <div class="flex flex-col rounded-lg relative" id="authorCard">
         <div class="flex flex-col justify-center py-4 mx-10 z-10">
-            <Image :src="imageUrl" alt="Image" width="120" style="border-radius: 6px;"></Image>
-            <div class="mt-1 text-center text-lg font-bold">面包etc</div>
+            <Image :src="imageUrl" alt="Image" width="120"></Image>
+            <div class="mt-1 text-center text-lg">面包etc</div>
         </div>
-        <div class="px-5 flex flex-col justify-center">
-
-
+        <div class="px-5 z-10 flex flex-col text-md">
+            <Button v-for="route in routes" :label="route.title" :icon="route.icon" @click="handleNavigate(route.path)" />
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
 import Image from 'primevue/image';
+import Button from 'primevue/button';
 
 const imageUrl: string = "https://www.freeimg.cn/i/2024/05/19/664a1a0f4b164.jpg";
+const router = useRouter();
+const routes = ref([
+    { path: "/", title: "随笔", icon: "pi pi-pencil" },
+    { path: "/contact", title: "留言", icon: "pi pi-megaphone" },
+    { path: "/about", title: "关于", icon: "pi pi-heart" }
+]);
+const selectedRoute: object = routes.value[0];
+
+const handleNavigate = (path: string): void => {
+    router.push(path);
+}
+
+const websiteButtons = ref([
+
+])
 </script>
 
-<style scoped></style>
+<style scoped>
+#authorCard {
+    position: relative;
+    overflow: hidden;
+    /* 确保模糊背景不会溢出 */
+    height: 95vh;
+}
+
+#authorCard::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url(../assets/images/bg-authorCard.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    filter: blur(5px);
+    /* 调整模糊效果的程度 */
+    z-index: 1;
+    /* 确保背景层在文字后面 */
+}
+
+#authorCard>* {
+    position: relative;
+    z-index: 10;
+    /* 确保文字和其他内容在模糊背景层上面 */
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@
 export default defineNuxtConfig({
   app: {
     head: {
+      title: "面包etc的博客",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" }
+      ],
       link: [
+        { rel: "icon", type: "image/x-icon", href: "/public/favicon.ico" },
         { rel: "stylesheet", href: "https://cdn.bootcdn.net/ajax/libs/lxgw-wenkai-webfont/1.6.0/style.min.css" },
       ]
     }
@@ -24,6 +30,7 @@ export default defineNuxtConfig({
     }
   },
   css: [
+    // 全局样式表 [会进行打包处理]
     "./assets/styles/style.css",
     "primevue/resources/themes/aura-light-green/theme.css"
   ]
