1ec6f1060ab3cb357ca3f228b1c1574321edca38
bread_etc <1054115624@qq.com>
2025-07-30 01:12:50 +0800

    refactor: delete primeVue, add shadcn-vue; change PingFangSC to NotoSansSC; using Tailwindcss V3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,15 @@
-import Aura from "@primevue/themes/aura";
-
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
-  devtools: { enabled: false },
-  
-  typescript: {
-    shim: false,
-    strict: true,
+  css: [
+    "assets/css/main.css",
+    "assets/css/tailwind.css",
+    "assets/css/transition.css",
+  ],
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@vueuse/nuxt"],
+  vueuse: {
+    ssrHandlers: true,
   },
-  app: {
-    head: {
-      charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1",
-      htmlAttrs: { lang: "zh-CN" },
-      titleTemplate: "Bread_etc 's blog" || "%s",
-      meta: [
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { name: "description", content: "Bread-etc's Blog" },
-        { name: "keywords", content: "前端, 博客, blog" },
-      ],
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-    },
-    pageTransition: { name: "page", mode: "out-in" },
-    layoutTransition: { name: "layout", mode: "out-in" },
-  },
-  css: ["assets/styles/main.css", "assets/styles/transition.css"],
 
+  // --- 运行时配置
   runtimeConfig: {
     databaseUrl: process.env.DATABASE_URL,
     jwtSecret: process.env.JWT_SECRET,
@@ -34,53 +18,11 @@ export default defineNuxtConfig({
     },
   },
 
+  // --- 后端服务器配置 ---
   nitro: {
     compressPublicAssets: {
       gzip: true,
       brotli: true,
     },
-  },
-
-  vite: {
-    esbuild: {
-      drop: ["console", "debugger"],
-    },
-  },
-
-  modules: [
-    "nuxt-lodash",
-    "@pinia/nuxt",
-    "@nuxtjs/tailwindcss",
-    "@primevue/nuxt-module",
-    "@vueuse/nuxt",
-  ],
-
-  lodash: {
-    prefix: "use",
-    prefixSkip: ["string"],
-  },
-
-  tailwindcss: {
-    viewer: false,
-    configPath: "./tailwind.config.ts",
-  },
-
-  primevue: {
-    options: {
-      ripple: true,
-      inputVariant: "filled",
-      theme: {
-        preset: Aura,
-        options: {
-          preset: "Prime",
-          darkModeSelectors: "system",
-          cssLayer: false,
-        },
-      },
-    },
-  },
-
-  vueuse: {
-    ssrHandlers: true,
   },
 });
